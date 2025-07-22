2e50a355271f87c3ad43c15397ff57c3e1c5e8ac
paris05494 <140006086+paris05494@users.noreply.github.com>
2025-07-22 14:07:01 +0700

    Update +page.svelte

--- Wiring_Harness/src/routes/+page.svelte
+++ Wiring_Harness/src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import StatusPanel from '$lib/components/StatusPanel.svelte';
-	import ExcelFileSelector from '$lib/components/ExcelFileSelector.svelte';
-	import HypertacVisualizer from '$lib/components/HypertacVisualizer.svelte';
-	import SignalInfoPanel from '$lib/components/SignalInfoPanel.svelte';
-	import HypertacModal from '$lib/components/Modal/HypertacModal.svelte';
+	import StatusPanel from '$lib/hypertac/StatusPanel.svelte';
+	import ExcelFileSelector from '$lib/hypertac/ExcelFileSelector.svelte';
+	import HypertacVisualizer from '$lib/hypertac/HypertacVisualizer.svelte';
+	import SignalInfoPanel from '$lib/hypertac/SignalInfoPanel.svelte';
+	import HypertacModal from '$lib/hypertac/Modal/HypertacModal.svelte';
 	import { appStore } from '$lib/stores/app-store';
 	import { Button } from 'flowbite-svelte';
-
 	let statusPanelCollapsed: boolean = true;
 	let showHypertacFullScreen: boolean = false;
+	import expandLogo from '$lib/assets/expandLogo.png';
 
 	// Control body overflow when modal is open
 	$: {
@@ -20,7 +20,6 @@
 			}
 		}
 	}
-
 	function openFullScreenHypertacView() {
 		if ($appStore.visualizationData?.hypertacSlots.length) {
 			showHypertacFullScreen = true;
@@ -31,18 +30,18 @@
 			);
 		}
 	}
-
 	function closeFullScreenHypertacView() {
 		showHypertacFullScreen = false;
-		console.log('Action: Modal close. showHypertacFullScreen set to FALSE.');
 	}
 </script>
 
 <div class="flex min-h-[calc(100vh-120px)] flex-col p-2 md:grid md:grid-cols-12 md:gap-2">
-	<div class="mb-4 flex h-full flex-col gap-2 md:col-span-3 md:mb-0">
+	<div class="mb-4 flex flex-col gap-2 md:col-span-3 md:mb-0">
 		<div
-			class="flex flex-col transition-all duration-700 ease-in-out
-                {statusPanelCollapsed ? 'h-auto min-h-[5rem]' : 'min-h-[150px] flex-grow'}"
+			class="flex flex-col transition-all duration-700 ease-in-out"
+			style={statusPanelCollapsed
+				? 'height: 5rem; max-height: 5rem; overflow: hidden;'
+				: 'flex-grow: 1; min-height: 150px; max-height: 40vh; overflow: hidden;'}
 		>
 			<StatusPanel bind:isCollapsed={statusPanelCollapsed} />
 		</div>
@@ -50,28 +49,24 @@
 			<ExcelFileSelector />
 		</div>
 	</div>
-
-	<div class="mb-4 flex flex-col md:col-span-6 md:mb-0">
-		<div class="panel flex h-full flex-col">
+	<div class="mb-4 flex flex-col gap-2 md:col-span-9 md:mb-0">
+		<div class="panel flex flex-grow-[2] flex-col">
 			<div class="mb-4 flex items-center justify-between">
 				<h2 class="text-2xl font-bold text-[var(--color-primary-green)]">HIL Hypertac</h2>
 				<Button
 					onclick={openFullScreenHypertacView}
 					disabled={!$appStore.visualizationData?.hypertacSlots.length || $appStore.isLoading}
-					class="bg-[var(--color-primary-green)] text-[var(--color-text-light)] hover:bg-emerald-700"
+					class="border border-gray-300 p-1.5 hover:bg-gray-100"
 					size="sm"
 				>
-					Open Full-Screen View
+					<img src={expandLogo} alt="Expand Logo" class="h-5 w-5" />
 				</Button>
 			</div>
-			<div class="relative flex-grow" style="min-height: 200px;">
+			<div class="scrollable-content relative flex-grow overflow-y-auto" style="min-height: 200px;">
 				<HypertacVisualizer isMainView={true} />
 			</div>
 		</div>
-	</div>
-
-	<div class="md:col-span-3">
-		<SignalInfoPanel />
+		<div class="flex flex-grow-[1] flex-col"><SignalInfoPanel /></div>
 	</div>
 </div>
 
